56c55f346544cd470e7b418d86865d27c0332fd2
bettaibi <bettaibinidhal00@gmail.com>
2021-03-11 15:40:54 +0100

    added by nidhal

--- implement_strstr.js
+++ implement_strstr.js
@@ -1,37 +1,42 @@
 
 /** STR STR */
 
-// function strStr(haystack, needle) {
-//     return haystack.indexOf(needle);
-// };
+function strStr(haystack, needle) {
+    return haystack.indexOf(needle);
+};
 
 
-// let haystack = "ss";
-// let needle = "ff";
-// console.log(strStr(haystack, needle) )
+let haystack = "ss";
+let needle = "ff";
+console.log(strStr(haystack, needle) )
 
 
 /** Repeating Subtring pattern */
 
-// function repeatedSubstringPattern(s) {
-//     const substr = extractsubstr(s);
-//     if(!substr){
-//         return false;
-//     }
-//     let arr = s.split(substr);
-//     for(let item of arr){
-//         if(item !== ''){
-//             return false;
-//         }
-//     }
-//     return true;
-// };
+function repeatedSubstringPattern(s) {
+    let leng = s.length;
+
+    for(let i=0; i< leng-1; i++){
+        
+        let substr = s.substring(0, i+1);
+
+        if(leng % substr.length === 0){
+
+            let iteration = leng / substr.length;
+            let newStr = '';
+            while(iteration > 0){
+                newStr += substr;
+                iteration--;
+            }
+            
+            if(newStr === s) return true;
+        }
+    }
+
+    return false;
+};
 
 
-function extractsubstr(s){
-  
-}
+const test = "ababababa";
 
-
-const test = "abaabaaba";
-console.log(extractsubstr(test))
+console.log(repeatedSubstringPattern(test))
