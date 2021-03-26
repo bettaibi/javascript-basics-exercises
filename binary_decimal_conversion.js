48bdee32c03f4dbcd643f7cf6e6ce8f90933c699
bettaibi <bettaibinidhal00@gmail.com>
2021-03-26 11:16:44 +0100

    added by nidhal

---
+++ binary_decimal_conversion.js
@@ -0,0 +1,40 @@
+//  Decimal to Binary
+function decimalToBinary(decimal){
+    let str = "";
+    
+    while(decimal >= 1){
+        let r = Math.trunc(decimal % 2);
+        str = r+str;
+        decimal = Math.trunc(decimal / 2);
+    }
+
+    return str;
+}
+
+
+// Binary to Decimal
+function binaryToDecimalMethod1(str){
+    // reverse str
+    let arr = str.split('').reverse();
+    let sum = 0;
+    for(let i=0; i< arr.length; i++){
+        if(arr[i] === '1'){
+            sum += Math.pow(2, i);
+        }
+    }
+    return sum;
+}
+
+// Binary to decimal without reversing
+function binaryToDecimalMethod2(str){
+    const leng = str.length;
+    let sum = 0;
+
+    for(let i=0; i< str.length; i++){
+        if(str.charAt(i) === '1'){
+            sum += Math.pow(2, leng -1 - i);
+        }
+    }
+
+    return sum;
+}
